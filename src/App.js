ad72237b9652553ee2b515a9ffd50fbd3fa1a9dd
niki-sal <ne.salehi@gmail.com>
2021-02-25 14:35:46 -0800

    Add filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import FruitContainer from './components/FruitContainer'
 
+
 function App() {
   const fruits = ['banana', 'watermelon', 'cherry', 'guava', 'apple', 'kiwi', 'strawberry', 'mango', 'pineapple', 'avocado'];
   return (
     <div>
       <h1>Fruit Filter</h1>
+      
       <FruitContainer fruits={fruits} />
     </div>
   );
--- src/components/FruitContainer.js
+++ src/components/FruitContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import List from './List';
+import Input from './Input'
 
 class FruitContainer extends Component {
     constructor(props) {
@@ -10,7 +11,23 @@ class FruitContainer extends Component {
         };
     }
 
+    handleFilter=(e) => {
+        e.preventDefault()
+        let filteredValue = e.target.value
+        // remove the fruits that don't contain the filteredValue (variable)
+        const fruitFilterList =  this.state.fruitsToDisplay.filter(fruitItem =>{
+            if(fruitItem.includes(filteredValue.toLowerCase())){
+                return true
+            }
+        })
+        this.setState({
+            fruitsToDisplay: fruitFilterList,
+            filterValue: filteredValue
+        })
+    }
+
     render() {
+
         console.log('---- state -----');
         console.log(this.state.fruitsToDisplay);
         console.log('---- props -----');
@@ -19,6 +36,7 @@ class FruitContainer extends Component {
 
         return (
             <div>
+                <Input value = {this.state.filterValue} handleFilter = {this.handleFilter} />
                 <List yoFruits={this.state.fruitsToDisplay}/>
             </div>
         )
